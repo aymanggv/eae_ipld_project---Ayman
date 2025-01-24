abd6137b8558e6e3ea840356e506499559229967
ayman gundru <aymanggv@hotmail.com>
2025-01-24 21:00:00 +0400

    added HR Analytics

---
+++ pages/2_👫_HR_Analytics_Dashboard.py
@@ -0,0 +1,48 @@
+import pandas as pd
+# Some extra libraries for date conversions and build the webapp
+import streamlit as st
+import streamlit.components.v1 as components
+
+from streamlit_pdf_viewer import pdf_viewer
+
+# ----- Page configs -----
+st.set_page_config(
+    page_title="Ayman's Portfolio",
+    page_icon="📊",
+)
+
+
+# ----- Left menu -----
+with st.sidebar:
+    st.write("Power BI dashboard developed to present HR related insights.")
+
+
+# ----- Title of the page -----
+st.title("💳 HR Analytics Dashboard")
+st.divider()
+
+st.header("Interactive Power BI View")
+
+# st.markdown(
+#     """
+#     <style>
+#     .iframe-container {
+#         text-align: left;
+#         margin-left: -212px;  /* Adjust this to move further to the left */
+#     }
+#     </style>
+#     <div class="iframe-container"> 
+#         <iframe src= "https://app.powerbi.com/view?r=eyJrIjoiMjlhNWNmNGEtZDg1YS00Nzg3LWEwZjgtOTZkYjE5MjMzMzk5IiwidCI6IjZmODM0MWEzLThlOWEtNDk0Mi04YWFmLTMxMWNhODExNjM4NCJ9" 
+#         width="1140" height="542" style="border:none;" allowfullscreen="true" scrolling="yes"></iframe>
+#     </div>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+components.iframe("https://app.powerbi.com/view?r=eyJrIjoiY2ViOTZmOGYtYmQwYi00MjdkLWJhNGUtZTY1NDRjOTdhMGY4IiwidCI6IjZmODM0MWEzLThlOWEtNDk0Mi04YWFmLTMxMWNhODExNjM4NCJ9", height=542)
+
+st.divider()
+
+st.header("PDF View")
+
+pdf_viewer("data/HR_Analytics.pdf")
