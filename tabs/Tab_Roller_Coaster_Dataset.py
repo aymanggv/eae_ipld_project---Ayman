1b1e337ecf769aefbce443916a5e25be51e8e631
ayman gundru <aymanggv@hotmail.com>
2025-05-14 02:04:33 +0400

    updated coaster tabs

--- tabs/Tab_Roller_Coaster_Dataset.py
+++ tabs/Tab_Roller_Coaster_Dataset.py
@@ -4,7 +4,7 @@ import pandas as pd
 def show():
     st.header("📂 Dataset")
     st.markdown("""
-                ##### ⚠️ Note: The original dataset has been reduced in size due to file size limitations on GitHub. This version includes a representative sample for analysis purposes. 
+                ##### Roller Coaster Dataset. 
                 ---
                 """)
     # st.markdown("""
@@ -15,13 +15,12 @@ def show():
 
     @st.cache_data
     def load_data():
-        st.write("First 1000 rows of the dataset")
-        data_path = "data/2015_Street_Tree_Census_-_Tree_Data_20250412.csv"
+        data_path = "data/coaster_db.csv"
 
         temps_df = pd.read_csv(data_path, low_memory=False) 
 
         return temps_df  # a Pandas DataFrame
 
-    tree_census = load_data()
+    df = load_data()
 
-    st.dataframe(tree_census.head(1000))  
+    st.dataframe(df.head(1000))  
