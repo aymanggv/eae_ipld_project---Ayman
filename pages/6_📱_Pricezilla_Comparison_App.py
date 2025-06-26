0dedf91048abe35771f70880868931be7d819f9a
ayman gundru <aymanggv@hotmail.com>
2025-06-27 00:05:43 +0400

    amde changeas pricezilla

--- pages/6_📱_Pricezilla_Comparison_App.py
+++ pages/6_📱_Pricezilla_Comparison_App.py
@@ -15,52 +15,12 @@ st.set_page_config(
 
 # ----- Left menu -----
 with st.sidebar:
-    st.write("Tableau dashboard developed to present iGaming related insights.")
+    st.write("A barcode-based price comparison Android app designed to help users compare product prices.")
     st.write("###")
     st.write("**Author:** Ayman")
 
 
 # ----- Title of the page -----
-st.title("🎰 iGaming Analysis Dashboard")
+st.title("📱 Pricezilla – A Bar-code Based Price Comparison App")
 st.divider()
 
-st.header("Interactive Tableau View")
-
-# st.markdown(
-#     """
-#     <style>
-#     .iframe-container {
-#         text-align: left;
-#         margin-left: -212px;  /* Adjust this to move further to the left */
-#     }
-#     </style>
-#     <div class="iframe-container">
-#         <iframe src="https://app.powerbi.com/view?r=eyJrIjoiYjlmN2Y0NDgtNmUxZS00YTVmLWFmYzgtMzBjODlmYjhiYWZlIiwidCI6IjZmODM0MWEzLThlOWEtNDk0Mi04YWFmLTMxMWNhODExNjM4NCJ9" 
-#         width="1140" height="542" style="border:none;" allowfullscreen="true" scrolling="yes"></iframe>
-#     </div>
-#     """,
-#     unsafe_allow_html=True
-# )
-
-# components.iframe("https://public.tableau.com/views/Book1_17469997268340/PerformanceOverview?:showVizHome=no&:embed=true", 
-#                 height=830,
-#                 width=1500)
-
-components.html(
-    """
-    <div style="transform: scale(0.65); transform-origin: top left; width: 2000px; height: 1100px; overflow: hidden;">
-        <iframe src="https://public.tableau.com/views/Book1_17469997268340/PerformanceOverview?:showVizHome=no&:embed=true"
-                width="1500" height="1000" style="border: none;">
-        </iframe>
-    </div>
-    """,
-    height=850,  # Adjust this to control the visible height in Streamlit
-    scrolling=False
-)
-
-
-# st.divider()
-
-# st.header("PDF View")
-
-# pdf_viewer("data/Airport Analysis.pdf")
