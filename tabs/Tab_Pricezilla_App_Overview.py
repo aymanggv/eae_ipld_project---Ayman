e14b463e9db5b34a400b6e6c8bc693412cd009b6
ayman gundru <aymanggv@hotmail.com>
2025-08-01 19:12:25 +0400

    updated overview pricezilla

--- tabs/Tab_Pricezilla_App_Overview.py
+++ tabs/Tab_Pricezilla_App_Overview.py
@@ -15,11 +15,9 @@ def show():
 # )
 
     
-    st.markdown(""" 
-    # 📱 Pricezilla – A Bar-code Based Price Comparison App
-
+    st.markdown("""  
     **A Final Year Individual Project**  
-    **Author:** Ayman Gundru
+    **Author:** Ayman Gundru  
     **Institution:** Middlesex University  
     **Module:** CST3515 – Embedded Linux System and Application Development
 
